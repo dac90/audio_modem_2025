c05277d8e7b326435a00cb07607d0e1413af022e
Jon Tindal <jon.tindal@outlook.com>
2025-05-28 17:35:58 +0100

    Update method for making longer samples

--- src/modem/main.py
+++ src/modem/main.py
@@ -1,19 +1,27 @@
 import numpy as np
 import numpy.typing as npt
 
-from modem import chirp, qpsk, wav
+from modem import chirp, pilot, qpsk, wav
 from modem.constants import BYTES_BLOCK_LENGTH
 
 
-def encode_data(data: bytes, wav_name: str):
-    qpsk_values = qpsk.qpsk_encode(data)
-    print(f"{qpsk_values.shape = }")
-    ofdm_symbols = qpsk.encode_ofdm_symbol(qpsk_values)
+def encode_data(data: bytes, wav_name: str) -> npt.NDArray[np.float64]:
+    data_bits = np.unpackbits(np.frombuffer(data, dtype=np.uint8))
+    data_qpsk_values = qpsk.qpsk_encode(data_bits)
+    data_ofdm_symbols = qpsk.encode_ofdm_symbol(data_qpsk_values)
+
+    num_ofdm_blocks = data_ofdm_symbols.shape[0]
+    print(f"Data encoded into {num_ofdm_blocks} OFDM blocks")
+
+    pilot_ofdm_symbols = pilot.generate_pilot_blocks(num_ofdm_blocks + 1)
+    ofdm_symbols = pilot.interleave_pilot_blocks(data_ofdm_symbols,pilot_ofdm_symbols)
 
     signal = np.concatenate((chirp.START_CHIRP, ofdm_symbols.flatten(), chirp.END_CHIRP))
-
     wav.generate_wav(wav_name, signal)
 
+
+def decode_data(signal: npt.NDArray[np.float64]):
+    ...
 
 if __name__ == "__main__":
     rng = np.random.default_rng(seed=42)
--- src/modem/pilot.py
+++ src/modem/pilot.py
@@ -1,4 +1,14 @@
-from .constants import DATA_PER_PILOT
+import numpy as np
+from modem.constants import DATA_BLOCK_LENGTH, DATA_PER_PILOT
+from modem import qpsk
+
+def generate_pilot_blocks(num: int):
+    rng = np.random.default_rng(seed=42)
+    pilot_bits = rng.integers(2, size=DATA_BLOCK_LENGTH * 2 * num)
+    pilot_qpsk_symbols = qpsk.qpsk_encode(pilot_bits)
+    pilot_ofdm_symbols = qpsk.encode_ofdm_symbol(pilot_qpsk_symbols)
+    return pilot_ofdm_symbols
+
 
 def interleave_pilot_blocks(data_blocks, pilot_blocks):
     result = []
@@ -14,4 +24,4 @@ def interleave_pilot_blocks(data_blocks, pilot_blocks):
                 if data_index < len(data_blocks):
                     result.append(data_blocks[data_index])
                     data_index += 1
-    return result
+    return np.array(result)
