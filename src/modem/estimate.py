0dd5c1fcec1890f02f6a6b07a4f057cf661b457f
Ido Balter <idobalter@gmail.com>
2025-05-28 17:47:54 +0100

    estimate noise variance and LLRs

--- src/modem/estimate.py
+++ src/modem/estimate.py
@@ -32,3 +32,43 @@ def estimate_channel(
     end_chirp_estimation: npt.NDArray[np.complex128],
     desired_estimate_index: int,
 ) -> npt.NDArray[np.complex128]: ...
+
+
+def estimate_noise_var(received_symbols: npt.NDArray[np.complex128],
+                       channel_gains: npt.NDArray[np.complex128],
+                       expected_symbols: npt.NDArray[np.complex128],):
+    # estimate the noise variance by computing the complex
+    #noise N = Y −ckX for each observation and averaging its squared real and imaginary
+    #parts.
+    noise = received_symbols - (channel_gains * expected_symbols)
+    noise_var = np.mean(np.square(np.real(noise))) + np.mean(np.square(np.imag(noise)))
+    return noise_var
+        
+def find_LLRs(
+    received_symbols: npt.NDArray[np.complex128],
+    channel_gains: npt.NDArray[np.complex128],
+    noise_var: float,
+) -> npt.NDArray[np.float64]:
+    """Calculate the Log-Likelihood Ratios (LLRs) for each symbol."""
+    # Calculate Equalised Y symbols, find real and imaginary, find conjugate of channel.
+    equalised_received = received_symbols / channel_gains
+    print("Equalised received symbols:", equalised_received)
+    real_equalised_received = np.real(equalised_received)
+    print(real_equalised_received)
+    imag_equalised_received = np.imag(equalised_received)
+    print(imag_equalised_received)
+    channel_gains_conjugate = np.conjugate(channel_gains)
+    print("Channel gains conjugate:", channel_gains_conjugate)
+    # Calculate the LLRs
+    llrs_real = np.real(np.sqrt(2)*channel_gains_conjugate*channel_gains*real_equalised_received / (noise_var)).astype(np.float64)
+    llrs_imag = np.real(np.sqrt(2)*channel_gains_conjugate*channel_gains*imag_equalised_received / (noise_var)).astype(np.float64)
+
+    return llrs_imag, llrs_real
+
+received_symbols = np.array([1+1j, 2+2j, 3+3j])
+channel_gains = np.array([1+0j, 0.5+0.5j, 1-1j])
+
+noise_var = 0.1
+lr,li = find_LLRs(received_symbols, channel_gains, noise_var)
+print("LLRs Real:", lr)
+print("LLRs Imaginary:", li)
