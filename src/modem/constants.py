913038e9db3ec8a0cc3b80a9d7de868893f929af
Ido Balter <idobalter@gmail.com>
2025-05-29 21:26:53 +0100

    added LDPC Constant

--- src/modem/constants.py
+++ src/modem/constants.py
@@ -26,3 +26,8 @@ BYTES_BLOCK_LENGTH = DATA_BLOCK_LENGTH // 4
 OFDM_SYMBOL_LENGTH = CYCLIC_PREFIX_LENGTH + FFT_BLOCK_LENGTH
 
 DATA_PER_PILOT = 1
+
+ldpc_standard = '802.11n'
+ldpc_rate = '1/2'
+ldpc_z_val = 81
+ldpc_ptype = 'A'
