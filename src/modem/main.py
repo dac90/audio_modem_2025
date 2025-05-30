66a893033aeb5870a1950dc510e87683d00c16a7
Ido Balter <idobalter@gmail.com>
2025-05-30 10:38:55 +0100

    implementing decoder function

--- src/modem/main.py
+++ src/modem/main.py
@@ -1,9 +1,14 @@
 import numpy as np
 import numpy.typing as npt
+import time
 
 from modem import chirp, pilot, qpsk, wav
 from modem.constants import BYTES_BLOCK_LENGTH
+from modem.chirp import synchronise   ###
+from modem.qpsk import recieve_signal ###
 
+
+### = Ido's addition 
 
 def encode_data(data: bytes, wav_name: str) -> npt.NDArray[np.float64]:
     data_bits = np.unpackbits(np.frombuffer(data, dtype=np.uint8))
@@ -18,9 +23,15 @@ def encode_data(data: bytes, wav_name: str) -> npt.NDArray[np.float64]:
 
     signal = np.concatenate((chirp.START_CHIRP, ofdm_symbols.flatten(), chirp.END_CHIRP))
     wav.generate_wav(wav_name, signal)
+    return signal, ofdm_symbols # ido's addition
+
+### Ido's implementation
+def decode_data(signal: npt.NDArray[np.float64], ofdm_symbols: npt.NDArray[np.float64]) -> None:
+    recv_signal = recieve_signal()
+    aligned_recv_signal = chirp.synchronise(recv_signal, sum(map(len, ofdm_symbols)), plot_correlations=True)
+    recv_ofdm_symbols = np.split(aligned_recv_signal[chirp.START_CHIRP.size:-chirp.END_CHIRP.size], len(ofdm_symbols))
 
 
-def decode_data(signal: npt.NDArray[np.float64]):
     ...
 
 if __name__ == "__main__":
