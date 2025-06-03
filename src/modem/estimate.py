5a535b4a6d02343e80b7c04620e8f0972f1253f2
Jon Tindal <jon.tindal@outlook.com>
2025-06-03 17:00:43 +0100

    Add option to plot error
    Useful to check if errors are Gaussian distributed

--- src/modem/estimate.py
+++ src/modem/estimate.py
@@ -1,11 +1,13 @@
 import numpy as np
 import numpy.typing as npt
+import matplotlib.pyplot as plt
 
 
 def estimate_snr(
     expected_symbols: npt.NDArray[np.complex128],
     received_symbols: npt.NDArray[np.complex128],
     channel_gains: npt.NDArray[np.complex128],
+    plot: bool = False,
 ) -> npt.NDArray[np.float64]:
     """Calculate the SNR for each frequency (ratio not dB).
     Note this function expects the received symbols before
@@ -13,6 +15,13 @@ def estimate_snr(
     # TODO: probably best to just use an input with unit power
     signal_power = np.abs(expected_symbols * channel_gains) ** 2
     error = (expected_symbols * channel_gains) - received_symbols
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.scatter(np.real(error), np.imag(error))
+        ax.set_title("Error $hX - Y$")
+        ax.set_aspect('equal')
+
     noise_power = np.nanmean(np.abs(error) ** 2)
     return signal_power / noise_power
 
