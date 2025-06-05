b6eef374360427297f13e9b9a3112bbb1c344b1b
Ido Balter <idobalter@gmail.com>
2025-06-05 15:06:19 +0100

    data per pilot + comments

--- src/modem/qpsk.py
+++ src/modem/qpsk.py
@@ -49,11 +49,11 @@ def qpsk_encode(bits: npt.NDArray[np.bool]) -> npt.NDArray[np.complex128]:
         Returns 1D array of QPSK constellation symbols of size (n * DATA_BLOCK_LENGTH),
         where n = (len(bits) // (2 * DATA_BLOCK_LENGTH))
     """
-    pad_len = int((-len(bits)) % (2 * DATA_BLOCK_LENGTH))
-    bits = np.pad(bits, (0, pad_len), constant_values=0) # FOR FUTURE : pad with non zeroez to test.
+    pad_len = int((-len(bits)) % (2 * DATA_BLOCK_LENGTH)) # pads the bits to make sure that they are a multiple of 2 * DATA_BLOCK_LENGTH
+    bits = np.pad(bits, (0, pad_len), constant_values=0) ##### FOR FUTURE : pad with non zeroez to test. #### pads the bits with zeroes according to the pad_len
 
-    bit_pairs = bits.reshape(-1, 2)
-    gray_indices = (bit_pairs[:, 0] << 1) | bit_pairs[:, 1]
+    bit_pairs = bits.reshape(-1, 2) # groups the bits into pairs of two.
+    gray_indices = (bit_pairs[:, 0] << 1) | bit_pairs[:, 1] 
 
     qpsk_constellation = np.array(
         [
@@ -72,10 +72,10 @@ def qpsk_decode(values: npt.NDArray[np.complex128],) -> bytes:
     """Decode received constellation symbols
     in the frequency domain into bytes using QPSK"""
     values = np.asarray(values)
-    bits_real = (np.real(values) < 0).astype(int)
-    bits_imag = (np.imag(values) < 0).astype(int)
-    bits = np.column_stack((bits_imag, bits_real)).reshape(-1)
-    bytes = np.packbits(bits).tobytes()
+    bits_real = (np.real(values) < 0).astype(int) # converts the real part of values to bits, if it is negative then it is 1, if it is positive then it is zero.
+    bits_imag = (np.imag(values) < 0).astype(int) # converts the imaginary part of values to bits, if it is negative then it is 1, if it is positive then it is zero.
+    bits = np.column_stack((bits_imag, bits_real)).reshape(-1) # stacks the bits together in pairs and reshapes them into a 1D array.
+    bytes = np.packbits(bits).tobytes() # converts the bits to bytes.
     return bytes
 import numpy as np
 
@@ -95,20 +95,20 @@ def encode_ofdm_symbol(
     # Output : Multiple arrays of QPSK symbols, each of with qpsk data of size QPSK_BLOCK_LENGTH_DATA and 
     #          zeroes of length POSITIVE_LOWER_BIN and POSITIVE_UPPER_BIN at the beginning and end of the array.
     assert qpsk_values.shape[0] % DATA_BLOCK_LENGTH == 0, f"QPSK values need to be an even multiple of OFDM length, got {qpsk_values.shape[0]}"
-    qpsk_data_blocks = qpsk_values.reshape(-1, DATA_BLOCK_LENGTH)
-    qpsk_blocks = np.zeros((qpsk_data_blocks.shape[0], FFT_BLOCK_LENGTH//2), dtype=np.complex128)
-    qpsk_blocks[:, POSITIVE_LOWER_BIN:POSITIVE_UPPER_BIN] = qpsk_data_blocks
-    qpsk_blocks = qpsk_blocks[:, 1:]
+    qpsk_data_blocks = qpsk_values.reshape(-1, DATA_BLOCK_LENGTH) # matrix with "DATA_block_LENGTH" Columns"
+    qpsk_blocks = np.zeros((qpsk_data_blocks.shape[0], FFT_BLOCK_LENGTH//2), dtype=np.complex128) # FIll the matrix with zeroes.
+    qpsk_blocks[:, POSITIVE_LOWER_BIN:POSITIVE_UPPER_BIN] = qpsk_data_blocks # Fill the matrix with the QPSK data blocks int he middle of the matrix.
+    qpsk_blocks = qpsk_blocks[:, 1:] # Remove the first column of zeroes, as it is not needed for the IFFT.
 
-    conj_blocks = np.conj(np.fliplr(qpsk_blocks))
+    conj_blocks = np.conj(np.fliplr(qpsk_blocks)) # conjugate and flip them 
     zero_col = np.zeros((np.shape(qpsk_blocks)[0], 1), dtype=np.complex128)
-    X = np.hstack([zero_col, qpsk_blocks, zero_col, conj_blocks])
+    X = np.hstack([zero_col, qpsk_blocks, zero_col, conj_blocks]) # create the full matrix with zeroes at bin 0 and N/2
 
-    x = np.fft.ifft(X, n=FFT_BLOCK_LENGTH)
-    x = np.hstack([x[:, -CYCLIC_PREFIX_LENGTH:], x])
+    x = np.fft.ifft(X, n=FFT_BLOCK_LENGTH) # take the IFFT to get time domain OFDM symbol
+    x = np.hstack([x[:, -CYCLIC_PREFIX_LENGTH:], x]) # Add cyclic prefix to the OFDM symbol
     np.testing.assert_allclose(np.imag(x), np.zeros_like(x), atol=1e-14)
 
-    return np.real(x)
+    return np.real(x) # return the real part in case we have small imaginary values due to numerical errors 
 # encode_ofdm_symbol(random_symbols)
 
 def decode_ofdm_symbol(
@@ -117,8 +117,8 @@ def decode_ofdm_symbol(
     """Decode time-domain OFDM symbol into
     constellation symbols in frequency-domain"""
     ofdm_symbol = ofdm_symbol[:, CYCLIC_PREFIX_LENGTH:]  # Discard cyclic prefix
-    freq_values = np.fft.fft(ofdm_symbol)
-    freq_values = freq_values[:, POSITIVE_LOWER_BIN:POSITIVE_UPPER_BIN]
+    freq_values = np.fft.fft(ofdm_symbol) # take the FFT to get frequency domain values
+    freq_values = freq_values[:, POSITIVE_LOWER_BIN:POSITIVE_UPPER_BIN] # select the positive frequencies. These are our data points.
 
     return freq_values
 
