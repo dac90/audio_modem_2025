061e06f9a7e3638837de59c35b479060d036fe6e
Jon Tindal <jon.tindal@outlook.com>
2025-06-05 13:46:16 +0100

    Update FFT bins limits to match standard

--- src/modem/constants.py
+++ src/modem/constants.py
@@ -14,8 +14,8 @@ FFT_BLOCK_LENGTH = 8192
 QPSK_BLOCK_LENGTH = (FFT_BLOCK_LENGTH - 2) // 2 #4095
 
 CYCLIC_PREFIX_LENGTH = 2048
-LOWER_FREQUENCY_BOUND = 1000
-UPPER_FREQUENCY_BOUND = 16000
+LOWER_FREQUENCY_BOUND = 1170
+UPPER_FREQUENCY_BOUND = 12557
 POSITIVE_LOWER_BIN = math.ceil(FFT_BLOCK_LENGTH*LOWER_FREQUENCY_BOUND/( FS)) # 170
 POSITIVE_UPPER_BIN = math.floor(FFT_BLOCK_LENGTH*UPPER_FREQUENCY_BOUND/( FS)) + 1 # 1535
 
@@ -36,4 +36,3 @@ corr_factor = 0.7 # should have a discussion about this value.
 ldpc_code = code(standard=ldpc_standard, rate=ldpc_rate, z=ldpc_z_val, ptype=ldpc_ptype)
 LDPC_INPUT_LENGTH = ldpc_code.K #972 currently
 LDPC_OUTPUT_LENGTH = ldpc_code.Nv #972 * 2 = 1944  # Rate 1/2
-
