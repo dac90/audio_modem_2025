6843cc729f3d498821e2921958255ee5aaddd148
Jon Tindal <jon.tindal@outlook.com>
2025-06-05 17:02:43 +0100

    Do not assume that a pilot block will be last

--- src/modem/pilot.py
+++ src/modem/pilot.py
@@ -18,8 +18,8 @@ def interleave_pilot_blocks(data_blocks, pilot_blocks):
         # Add a pilot block
         result.append(pilot_blocks[i])
         
-        # Add data blocks if it's not the last pilot
-        if i < len(pilot_blocks) - 1:
+        # Add data blocks
+        if i < len(pilot_blocks):
             for _ in range(DATA_PER_PILOT):
                 if data_index < len(data_blocks):
                     result.append(data_blocks[data_index])
